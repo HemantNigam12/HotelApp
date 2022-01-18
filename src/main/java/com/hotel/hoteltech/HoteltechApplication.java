188e65fadbbfa9de0afdb5e4296baf1eb5a1d101
HemantNigam12 <hemant@unyscape.com>
2022-01-18 14:46:37 +0530

    Initial version

---
+++ src/main/java/com/hotel/hoteltech/HoteltechApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.hoteltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteltechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/hoteltech/HoteltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.hoteltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
